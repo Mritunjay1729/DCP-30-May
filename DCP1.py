6536e176a3d6471af3ac22ce82fdd93f9373531a
vikaskhullar <vikas.khullar@gmail.com>
2021-05-30 22:56:43 +0530

    basics

---
+++ DCP1.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 21:32:25 2021
+
+@author: vikas
+"""
+
+print("Hi")
+
+print?
+
+help(print)
+
+print('vikas')
+
+print('vikas', 'Khullar')
+
+print('vikas', 'Khullar', sep='----')
+
+print('Vikas')
+print("Khullar")
+
+
+print('Vikas', 'Khullar', sep ='?',end='--')
+print("Khullar")
+
+
+import pandas
+import numpy as np
+import matplotlib as plt
+
+help(pandas)
+
+import pandas as pd
+help(pd)
+
+
+!pip install numpy
+!conda install numpy
+
+
+import pandas as pd
+
+pd.__version__
+
+import numpy as np
+
+np.__version__
+
+!pip list
+
+!pip show pandas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DCP2.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 22:18:07 2021
+
+@author: vikas
+"""
+
+print("hello world")
+
+country = "India"
+
+a = 10
+
+b= 10.4
+
+c = True
+
+d = False
+
+
+input()
+
+e = input()
+
+country = input("Enter Country Name ")
+
+print("I Live in ", country)
+
+print(f"I live in {country}")
+
+print("I live in {0}".format(country))
+
+
+a=10
+b=20
+
+print("Value of a is {0} and value of b is {1}".format(a,b))
+
+#Type conversion
+
+f=3.5
+i=5
+s='5.6'
+
+f_i = int(f)
+#f_s = str(8)
+
+s_f = float(s)
+s_i = int (float(s))
+
+
+var1 = int(input("Enter Var1  "))
+
+
+a_c = chr(10)
+a_c
+
+ord('Z')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
