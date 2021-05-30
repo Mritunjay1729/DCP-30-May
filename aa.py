63067a7cbf6bd3107379a3db4b7512e7910182dc
vikaskhullar <vikas.khullar@gmail.com>
2021-05-30 21:29:58 +0530

    Create aa.py

---
+++ aa.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 21:17:02 2021
+
+@author: vikas
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 18:33:35 2021
+
+@author: vikas
+"""
+
+a=10
+b=20
+c = a+b
+print(c)
